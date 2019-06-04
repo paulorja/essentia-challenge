cf3b2db423c5630cccb09ab5ac0989862b01fe0b
Paulo Jansen <pauloo.jansen@gmail.com>
2019-06-04 10:49:21 -0300

    adjusting detail screen

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_04_101041) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clientes", force: :cascade do |t|
     t.string "nome"
     t.string "email"
